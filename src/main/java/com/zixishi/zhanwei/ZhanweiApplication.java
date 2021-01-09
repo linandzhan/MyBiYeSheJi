2172f2124962a3b94542ac604c67bbc29d81f861
zhanweikai <zhanwk@ewhale.cn>
2021-01-09 12:56:22 +0800

    first commit

---
+++ src/main/java/com/zixishi/zhanwei/ZhanweiApplication.java
@@ -0,0 +1,13 @@
+package com.zixishi.zhanwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhanweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhanweiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zixishi/zhanwei/ZhanweiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zixishi.zhanwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhanweiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
